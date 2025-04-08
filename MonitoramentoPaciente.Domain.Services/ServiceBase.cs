946851a5e676a7c03bd897b929d4948f3395b29b
PedroVMB <pedromb.dev@gmail.com>
2025-04-07 21:32:32 -0300

    feat: adicionando o ServiceBase da camada de servico do dominio

---
+++ MonitoramentoPaciente.Domain.Services/ServiceBase.cs
@@ -0,0 +1,39 @@
+﻿using MonitoramentoPaciente.Domain.Core.Interfaces.Repositorys;
+using MonitoramentoPaciente.Domain.Core.Interfaces.Services;
+
+namespace MonitoramentoPaciente.Domain.Services;
+
+public class ServiceBase<TEntity> : IServiceBase<TEntity> where TEntity : class
+{
+    private readonly IRepositoryBase<TEntity> repository;
+
+    public ServiceBase(IRepositoryBase<TEntity> repository)
+    {
+        this.repository = repository;
+    }
+    
+    public void Add(TEntity entity)
+    {
+        repository.Add(entity);
+    }
+
+    public void Update(TEntity entity)
+    {
+        repository.Update(entity);
+    }
+
+    public void Delete(TEntity entity)
+    {
+        repository.Delete(entity);
+    }
+
+    public IEnumerable<TEntity> GetAll()
+    {
+        return repository.GetAll();
+    }
+
+    public TEntity GetById(int id)
+    {
+        return repository.GetById(id);
+    }
+}
